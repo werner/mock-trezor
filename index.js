c450c857591b2adeaa84d4c334f8bcfee144f08b
werner <werner@yahoo.es>
2018-07-20 14:39:05 -0300

    Add best case scenario

---
+++ index.js
@@ -0,0 +1,38 @@
+export default {
+  DeviceList: function(configUrl) {
+    return {
+      removeListener: function (name, callback) { },
+      on: function (name, callback) { name === 'transport' ? callback() : callback },
+      hasDeviceOrUnacquiredDevice: function () {
+        return true;
+      },
+      acquireFirstDevice: function (bool) {
+        return new Promise((resolve, reject) => {
+          resolve({ device: { features: { major_version: 1000, minor_version: 100, patch_version: 100  } }, 
+            session: {
+              on: function (name, callback) {  },
+              getPublicKey: function (path) {
+                return new Promise((resolve, reject) => { 
+                  resolve({ message: { xpub: 'tpubD6NzVbkrYhZ4YSh1zgHc1L2fNXQmSZM1FEbVFpNGzK9J1GDuhRnfoLUA7Unzq44qHVviVtyKdfLjnJYiuTUTjYAJt6Un4svFfRPb7m6TvZk' } });
+                })
+              },
+              signTx: function (inputs, outputs, transactions, coin) {
+                return new Promise((resolve, reject) => {
+                  resolve({
+                    message: {
+                      serialized: {
+                        serialized_tx: '000serializedTx',
+                        signatures: ['1', '2', '3']
+                      }
+                    }
+                  });
+                })
+              }
+            }
+          })
+          return new Promise(function (resolve, reject) {  })
+        }).catch(function () {  })
+      }
+    }
+  }
+}
