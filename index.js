dff97b54ba30747511da35e4083c1fd0d2f97ca3
werner <werner@yahoo.es>
2018-07-31 12:33:17 -0300

    Add xpub when coin is bitcoin

--- index.js
+++ index.js
@@ -11,9 +11,16 @@ module.exports = {
           resolve({ device: { features: { major_version: 1000, minor_version: 100, patch_version: 100  } }, 
             session: {
               on: function (name, callback) {  },
-              getPublicKey: function (path) {
+              getPublicKey: function (path, coin) {
                 return new Promise((resolve, reject) => { 
-                  resolve({ message: { xpub: 'tpubD6NzVbkrYhZ4YSh1zgHc1L2fNXQmSZM1FEbVFpNGzK9J1GDuhRnfoLUA7Unzq44qHVviVtyKdfLjnJYiuTUTjYAJt6Un4svFfRPb7m6TvZk' } });
+                  switch(coin) {
+                    case 'bitcoin':
+                      resolve({ message: { xpub: 'xpub661MyMwAqRbcG9mRZW2tccfkuRxUoTuw4j2B5m1hYGEVfHDxaH5cdie93W2fr4TfMiP3Yss5pYywx3JWu166AEwE5vXD9HcXSHqWZvp7xkF' } });
+                      break
+                    case 'testnet':
+                      resolve({ message: { xpub: 'tpubD6NzVbkrYhZ4YSh1zgHc1L2fNXQmSZM1FEbVFpNGzK9J1GDuhRnfoLUA7Unzq44qHVviVtyKdfLjnJYiuTUTjYAJt6Un4svFfRPb7m6TvZk' } });
+                      break
+                  }
                 })
               },
               signTx: function (inputs, outputs, transactions, coin) {
