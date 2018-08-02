b5f1b703a688acad3a80ca8ac2ebbe2f8a876e68
werner <werner@yahoo.es>
2018-08-02 10:46:13 -0300

    Add getPublicKey, ethereumGetAddress and signTransaction updated to TrezorConnect v5

--- index.js
+++ index.js
@@ -49,6 +49,43 @@ module.exports = {
           })
           return new Promise(function (resolve, reject) {  })
         }).catch(function () {  })
+      },
+      getPublicKey: function ({path, coin}) {
+        return new Promise((resolve, reject) => { 
+          switch(coin) {
+            case 'bitcoin':
+              resolve({ success: true, payload: { xpub: 'xpub661mymwaqrbcg9mrzw2tccfkurxuotuw4j2b5m1hygevfhdxah5cdie93w2fr4tfmip3yss5pyywx3jwu166aewe5vxd9hcxshqwzvp7xkf' } });
+              break
+            case 'testnet':
+              resolve({ success: true, payload: { xpub: 'tpubD6NzVbkrYhZ4YSh1zgHc1L2fNXQmSZM1FEbVFpNGzK9J1GDuhRnfoLUA7Unzq44qHVviVtyKdfLjnJYiuTUTjYAJt6Un4svFfRPb7m6TvZk' } });
+              break
+            default:
+              resolve({ success: true, payload: { xpub: 'xpub661mymwaqrbcg9mrzw2tccfkurxuotuw4j2b5m1hygevfhdxah5cdie93w2fr4tfmip3yss5pyywx3jwu166aewe5vxd9hcxshqwzvp7xkf' } });
+              break
+          }
+        })
+      },
+      ethereumGetAddress: function ({path, coin}) {
+        return new Promise((resolve, reject) => {
+          resolve({
+            success: true,
+            payload: {
+              path: '44,37310,0,0,0',
+              address: 'b5ae11144f988735aecf469b96b72f979736dbcc'
+            }
+          })
+        })
+      },
+      signTransaction: function({ inputs, outputs, coin }) {
+        return new Promise((resolve, reject) => {
+          resolve({
+            success: true,
+            payload: {
+              serializedTx: '000serializedTx',
+              signatures: ['1', '2', '3']
+            }
+          });
+        })
       }
     }
   }
